bbf9c924cdd61f6d465f8f8ed5331ef910db12e4
mrspaiva <natyap28@hotmail.com>
2022-04-16 18:21:56 -0300

    FEAT: Add navigation to welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../../types/RootStackParams';
 
 // import { Container } from './styles';
 
-const Welcome: React.FC = () => {
+type welcomeScreenProp = StackNavigationProp<RootStackParamList, 'Welcome'>;
+
+const Welcome = () => {
+  const navigation = useNavigation<welcomeScreenProp>();
+
   return (
     <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Homepage')}>
         <Text>Solar System</Text>
       </TouchableOpacity>
     </View>
