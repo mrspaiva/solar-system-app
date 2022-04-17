6eaeafb9930113e0b35d932b1516a5dd6cf7fb19
mrspaiva <natyap28@hotmail.com>
2022-04-17 16:23:56 -0300

    FEAT: Status bar config

--- src/routes/stackRoute.tsx
+++ src/routes/stackRoute.tsx
@@ -5,12 +5,14 @@ import {RootStackParamList} from '../types/RootStackParams';
 
 import Welcome from '../pages/Welcome';
 import BottomTabsRoute from './bottomTabsRoute';
+import {StatusBar} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function StackRoute() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle={'light-content'} />
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Homepage" component={BottomTabsRoute} />
