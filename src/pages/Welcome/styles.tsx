eeb53eae6e1c2f114607cb956623d5895c823fe8
mrspaiva <natyap28@hotmail.com>
2022-04-21 13:17:30 -0300

    STYLE: Create button container and fix layout

--- src/pages/Welcome/styles.tsx
+++ src/pages/Welcome/styles.tsx
@@ -2,8 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  flex-direction: row;
 `;
 
 export const Background = styled.Image.attrs({
@@ -11,6 +10,14 @@ export const Background = styled.Image.attrs({
 })`
   background-color: #000;
   position: absolute;
+`;
+
+export const ContentWrapper = styled.View`
+  height: 60%;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  align-self: flex-end;
 `;
 
 export const TitleSection = styled.View`
@@ -31,4 +38,16 @@ export const Title = styled.Text`
   font-size: 32px;
   color: #fff;
   text-align: center;
+`;
+
+export const NavigationSection = styled.View`
+  margin-bottom: 70px;
+`;
+
+export const NavigationText = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 16px;
 `;
